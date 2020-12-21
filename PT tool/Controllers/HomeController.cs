abafaf5b4e228730fb460cbcaa88946c6187aeb6
kongmengfei <57654821+kongmengfei@users.noreply.github.com>
2020-12-21 17:47:17 +0800

    Add project files.

---
+++ PT tool/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PT_tool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Settings(string token)
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectResult SetSettings(string token)
+        {
+            ViewBag.Message = "This is Settings page.";
+            HttpContext.Session["token"] = "Bearer " + token;
+
+            return RedirectResult
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
