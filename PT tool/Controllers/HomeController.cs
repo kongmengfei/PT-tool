44f3c625cc7bee3b0b8f1293464ffae526781f86
Baker Kong_MSFT <57654821+kongmengfei@users.noreply.github.com>
2020-12-21 22:19:02 +0800

    jjj

--- PT tool/Controllers/HomeController.cs
+++ PT tool/Controllers/HomeController.cs
@@ -13,21 +13,28 @@ namespace PT_tool.Controllers
             return View();
         }
 
-        
-        public ActionResult Settings(string token)
+        [HttpGet]
+        public ActionResult Settings()
         {
-            
+            if (HttpContext.Session["token"] is null || string.IsNullOrEmpty( HttpContext.Session["token"].ToString()))
+            {
+                ViewBag.token = "Please Enter your Token !";
+            }
+            else
+            {
+                ViewBag.token = HttpContext.Session["token"];
+            }
 
             return View();
         }
 
         [HttpPost]
-        public RedirectResult SetSettings(string token)
+        public ActionResult SetSettings(string token)
         {
             ViewBag.Message = "This is Settings page.";
             HttpContext.Session["token"] = "Bearer " + token;
 
-            return RedirectResult
+            return RedirectToAction("GetRangQuestion", "Question");
         }
 
         public ActionResult Contact()
